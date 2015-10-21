187a4ac82374cf9bd510bf87025a1ecbac3ab63e
Markus Sattler <help.markus+git@gmail.com>
2015-10-21 17:17:09 +0200

    bump version optimize String usage (less malloc / realloc) remove double debug line

--- src/WebSocketsClient.cpp
+++ src/WebSocketsClient.cpp
@@ -272,25 +272,22 @@ void WebSocketsClient::sendHeader(WSclient_t * client) {
 
     unsigned long start = micros();
 
-    String handshake;
-	handshake="GET "+client->cUrl+" HTTP/1.1\r\n";
-    handshake+="Host: "+_host+"\r\n";
-	handshake+="Upgrade: websocket\r\n";
-	handshake+="Connection: Upgrade\r\n";
-	handshake+="User-Agent: arduino-WebSocket-Client\r\n";
-	handshake+="Sec-WebSocket-Version: 13\r\n";
-	handshake+="Sec-WebSocket-Protocol: arduino\r\n";
-	handshake+="Sec-WebSocket-Key: "+client->cKey+"\r\n";
-	
-	if(client->cExtensions.length() > 0) {
-		handshake+="Sec-WebSocket-Extensions: "+client->cExtensions+"\r\n";
+    String handshake =  "GET " + client->cUrl + " HTTP/1.1\r\n"
+                        "Host: " + _host + "\r\n"
+                        "Upgrade: websocket\r\n"
+                        "Connection: Upgrade\r\n"
+                        "User-Agent: arduino-WebSocket-Client\r\n"
+                        "Sec-WebSocket-Version: 13\r\n"
+                        "Sec-WebSocket-Protocol: arduino\r\n"
+                        "Sec-WebSocket-Key: " + client->cKey + "\r\n";
+
+    if(client->cExtensions.length() > 0) {
+        handshake += "Sec-WebSocket-Extensions: " + client->cExtensions + "\r\n";
     }
-	
-	handshake+="\r\n";
-	client->tcp.write(handshake.c_str(), handshake.length());
 
-    DEBUG_WEBSOCKETS("[WS-Client][sendHeader] sending header... Done (%uus).\n", (micros() - start));
+    handshake += "\r\n";
 
+    client->tcp.write(handshake.c_str(), handshake.length());
 
     DEBUG_WEBSOCKETS("[WS-Client][sendHeader] sending header... Done (%uus).\n", (micros() - start));
 
