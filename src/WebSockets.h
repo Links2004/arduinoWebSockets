7361e2b1b66b9bbd9cdb0beeac06676cb78647f2
Sergey Anisimov <anisimov.sergey@gmail.com>
2016-07-15 20:29:13 +0100

    WEBSOCKETS_NETWORK_TYPE exteral definition
    Making possible to define WEBSOCKETS_NETWORK_TYPE as a compile parameter.

--- src/WebSockets.h
+++ src/WebSockets.h
@@ -52,12 +52,14 @@
 // max size of the WS Message Header
 #define WEBSOCKETS_MAX_HEADER_SIZE  (14)
 
+#if !defined(WEBSOCKETS_NETWORK_TYPE) 
 // select Network type based
 #if defined(ESP8266) || defined(ESP31B)
 #define WEBSOCKETS_NETWORK_TYPE NETWORK_ESP8266
 //#define WEBSOCKETS_NETWORK_TYPE NETWORK_ESP8266_ASYNC
 #else
 #define WEBSOCKETS_NETWORK_TYPE NETWORK_W5100
+#endif
 #endif
 
 #if (WEBSOCKETS_NETWORK_TYPE == NETWORK_ESP8266_ASYNC)
