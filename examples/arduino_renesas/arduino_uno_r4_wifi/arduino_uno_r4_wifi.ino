59289b110dae655d50392a2493ba77ebdae37e85
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2024-07-28 20:53:17 +0700

    feat: read me updated. example fixed.

--- examples/arduino_renesas/arduino_uno_r4_wifi/arduino_uno_r4_wifi.ino
+++ examples/arduino_renesas/arduino_uno_r4_wifi/arduino_uno_r4_wifi.ino
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #include "WiFiS3.h"
 #include <WebSocketsClient.h>
@@ -8,41 +10,49 @@
 
 WebSocketsClient webSocket;
 
+size_t _printf(const char *format, ...) {
+  va_list arg;
+  va_start(arg, format);
+  size_t ret = vprintf(format, arg);
+  va_end(arg);
+  return ret;
+}
+
 #define USE_SERIAL Serial
 
 void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
 	const uint8_t* src = (const uint8_t*) mem;
-	USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	_printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
 	for(uint32_t i = 0; i < len; i++) {
 		if(i % cols == 0) {
-			USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+			_printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
 		}
-		USE_SERIAL.printf("%02X ", *src);
+		_printf("%02X ", *src);
 		src++;
 	}
-	USE_SERIAL.printf("\n");
+	_printf("\n");
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
 	switch(type) {
 		case WStype_DISCONNECTED:
-			USE_SERIAL.printf("[WSc] Disconnected!\n");
+			_printf("[WSc] Disconnected!\n");
 			break;
 		case WStype_CONNECTED:
-			USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+			_printf("[WSc] Connected to url: %s\n", payload);
 
 			// send message to server when Connected
 			webSocket.sendTXT("Connected");
 			break;
 		case WStype_TEXT:
-			USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+			_printf("[WSc] get text: %s\n", payload);
 
 			// send message to server
 			// webSocket.sendTXT("message here");
 			break;
 		case WStype_BIN:
-			USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+			_printf("[WSc] get binary length: %u\n", length);
 			hexdump(payload, length);
 
 			// send data to server
@@ -71,7 +81,7 @@ void setup() {
 	USE_SERIAL.println();
 
 	for(uint8_t t = 4; t > 0; t--) {
-		USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+		_printf("[SETUP] BOOT WAIT %d...\n", t);
 		USE_SERIAL.flush();
 		delay(1000);
 	}
