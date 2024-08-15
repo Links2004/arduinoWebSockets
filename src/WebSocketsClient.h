c2796733f03075b4edd1c03e8077135b0a22b662
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2024-08-16 05:10:54 +0700

    SSL support for Uno 4 and bugfixes

--- src/WebSocketsClient.h
+++ src/WebSocketsClient.h
@@ -140,7 +140,7 @@ class WebSocketsClient : protected WebSockets {
 #define SSL_FINGERPRINT_NULL NULL
 #endif
 
-#if defined(WIO_TERMINAL) || defined(SEEED_XIAO_M0)
+#if defined(WIO_TERMINAL) || defined(SEEED_XIAO_M0) || defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT)
     const char * _CA_cert;
     const uint8_t * _CA_bundle;
 #endif
